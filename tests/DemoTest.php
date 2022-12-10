7d4a86b4f0e807536bcea235606bd817e79c09a9
Maxim Linenko <linenko.maxim@gmail.com>
2022-12-10 18:36:39 +0200

    setup project

---
+++ tests/DemoTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Maxlzp\ApiClient\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class DemoTest extends TestCase
+{
+    public function setUp(): void
+    {
+    }
+
+    /**
+     * @test
+     */
+    public function shouldPass(): void
+    {
+        $this->assertTrue(true);
+    }
+}
